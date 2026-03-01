548c9e6e2774c3d862f7d1cde5dd710003393840
Francisco Navarro <franng96@hotmail.com>
2026-03-01 09:55:50 +0100

    queries terminadas

--- queries.py
+++ queries.py
@@ -24,3 +24,28 @@ series=Serie.objects.filter(
 sciFi=Serie.objects.exclude(
     categoria__icontains='Sci-Fi'
 ).order_by('-titulo')
+
+# Ejercicio 7
+promedio=Serie.objects.aggregate(precio_medio=Avg('precio_suscripcion'))
+
+# Ejercicio 8
+dirigidas=Director.objects.annotate(num_series=Count('series'))
+
+# Ejercicio 9
+serie = Serie.objects.order_by('-capitulos__duracion_minutos').first()
+
+# Ejercicio 10
+capitulos=Capitulo.objects.select_related("serie__director")
+for c in capitulos:
+    print(
+        c.titulo,
+        c.serie.titulo,
+        c.serie.director.nombre
+    )
+
+# Ejercicio 11
+directores=Director.objects.prefetch_related('series')
+for d in directores:
+    print(d.nombre)
+    for serie in d.series.all():
+        print("  -", serie.titulo)
